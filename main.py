761b0a7cab4dc46237eb9fd61622a6fe3e02286d
Liam <liam.gibbings@cloudsolutions.co.uk>
2016-09-08 15:23:23 +0100

    Implemented method of getting starred tracks

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 import spotipy
 import spotipy.util as sutil
+import sys
 
-def get_token():
-    scope = 'user-library-read user-library-modify'
-    user = 'gibbings_lfc'
+def get_username():
+    if len(sys.argv) > 1:
+        user = sys.argv[1]
+    else:
+        user = raw_input("Enter your username: ")
+    return user
+
+def get_token(user):
+    scope = 'playlist-read-private'
     clientid = '26637fb0e85141fcb215898a91eb5a84'
     clientsecret = 'c2f01d25d82f4e42941cf1f7e503b643'
-    redirecturi = 'https://github.com/liamgbs/spotisync'
-    token = sutil.prompt_for_user_token(user, scope, clientid, clientsecret)
+    redirecturi = 'http://example.com/callback/'
+    token = sutil.prompt_for_user_token(user, scope, clientid, clientsecret, redirecturi)
     assert token
     return token
 
-sp = spotipy.Spotify(auth=get_token())
+def get_starred_tracks(sp, user):
+    starred = sp.user_playlist(user) # get starred playlist
+    tracks_paging = starred['tracks'] # extract paging object
+    tracks = tracks_paging['items'] # return list containing all tracks as playlist track objects
+    return tracks
+
+def main():
+    user = get_username()
+    sp = spotipy.Spotify(auth=get_token(user))
+    starred_tracks = get_starred_tracks(sp, user)
+    print starred_tracks
+
+
+if __name__ == "__main__" : main()
