546f226ef47d195d39f685f8d9a20f8c15b2e611
Liam <liam.gibbings@cloudsolutions.co.uk>
2016-09-08 17:39:20 +0100

    Added functionality to get ALL tracks from starred

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import spotipy
 import spotipy.util as sutil
 import sys
+from datetime import datetime, timedelta
+from pprint import pprint
 
 def get_username():
     if len(sys.argv) > 1:
@@ -21,13 +23,31 @@ def get_token(user):
 def get_starred_tracks(sp, user):
     starred = sp.user_playlist(user) # get starred playlist
     tracks_paging = starred['tracks'] # extract paging object
-    tracks = tracks_paging['items'] # return list containing all tracks as playlist track objects
-    return tracks
+    tracklist = []
+    while tracks_paging:
+        star_tracks = tracks_paging['items'] # list containing all tracks as playlist track objects
+        for playlisttrack in star_tracks:
+            tracklist.append( (playlisttrack['added_at'], str(playlisttrack['track']['id']), playlisttrack['track']['name']) )
+        tracks_paging = sp.next(tracks_paging)
+    return tracklist
+
+def is_track_older_than(track, age_days=7):
+    assert isinstance(track, tuple)
+    spotify_format = "%Y-%m-%dT%H:%M:%SZ"
+    track_date = datetime.strptime(track[0], spotify_format)
+    if not (datetime.now() - track_date).days > age_days: # if delta of todays date and track add date > 7 (?)
+        return True
+    else:
+        return False
 
 def main():
     user = get_username()
     sp = spotipy.Spotify(auth=get_token(user))
     starred_tracks = get_starred_tracks(sp, user)
-    print starred_tracks
+
+    for t in starred_tracks:
+        print t[2]
+        print is_track_older_than(t)
+    print len(starred_tracks)
 
 if __name__ == "__main__" : main()
